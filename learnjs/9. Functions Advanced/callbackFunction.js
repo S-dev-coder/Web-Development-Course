1dd2bef3f1c478e145a6da48820766c838ea0843
Sakshi Kumari <80236932+S-dev-coder@users.noreply.github.com>
2023-09-14 13:35:23 +0530

    CallBack Functions: Different Methods

--- learnjs/9. Functions Advanced/callbackFunction.js
+++ learnjs/9. Functions Advanced/callbackFunction.js
@@ -2,24 +2,25 @@ const calculate = (a, b, operation) => {
     return operation(a, b);
 };
 
-//// method 1
+//// method 1  - call back function
 const addition = calculate(3, 4, function (num1, num2) {
     return num1 + num2;
 });
 
 console.log(addition);
 
-// method 2
+// method 2- arrow function
 const subtraction = (a, b) => a - b;
 
 const subResult = calculate(8, 3, subtraction);
 
 console.log(subResult);
 
-//method 3
+//method 3-- normal declaration function
 function multiply(a, b) {
     return a * b;
 }
 
 const mulResult = calculate(3, 2, multiply);
 console.log(mulResult);
+  
