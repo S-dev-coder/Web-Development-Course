9027e7d3bbcd8b06a416c59e8c0d1c3db01bfeaf
Sakshi Kumari <80236932+S-dev-coder@users.noreply.github.com>
2023-09-14 13:34:16 +0530

    Arrow Functions

--- learnjs/9. Functions Advanced/arrowFunction.js
+++ learnjs/9. Functions Advanced/arrowFunction.js
@@ -1,10 +1,22 @@
+
+function greet(){
+    console.log("Hey !!");
+}
+greet();
+
+  // changing above normal function to the arrow function below
+
 const greet = (count) => {
-    for (let i = 0; i < count; i++) console.log("Hello World");
+  // taking statements 
+    for (let i = 0; i < count; i++) 
+    console.log("Hello World");
 };
 
 greet(3);
 
+
 const square = (num) => {
+    // taking expressions
     return num * num;
 };
 
